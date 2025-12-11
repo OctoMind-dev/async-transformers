5664f9f8fc1853c32d89384e73e6f979ce3a8a5f
Stefan Rinke <sker65@gmail.com>
2025-12-11 15:04:11 +0100

    tests

--- tests/handledRejectionPromise.spec.ts
+++ tests/handledRejectionPromise.spec.ts
@@ -2,14 +2,15 @@ import { HandledRejectionPromise } from "../src/handledRejectionPromise";
 
 describe(HandledRejectionPromise.name, () => {
   it("handles resolved Promises", async () => {
-    const handled = new HandledRejectionPromise(Promise.resolve(3));
+    const handled = new HandledRejectionPromise(Promise.resolve(3), 0);
 
-    await expect(handled.promise).resolves.toBe(3);
+    await expect(handled.promise).resolves.toEqual({ value: 3, index: 0 });
   });
 
   it("handles rejected Promises", async () => {
     const handled = new HandledRejectionPromise(
-      Promise.reject(new Error("oopsie"))
+      Promise.reject(new Error("oopsie")),
+      0
     );
 
     await expect(handled.promise).rejects.toThrowErrorMatchingInlineSnapshot(
@@ -19,17 +20,19 @@ describe(HandledRejectionPromise.name, () => {
 
   it("handles Promises that resolve (later)", async () => {
     const handled = new HandledRejectionPromise(
-      new Promise((resolve) => setTimeout(() => resolve(4), 1))
+      new Promise((resolve) => setTimeout(() => resolve(4), 1)),
+      0
     );
 
-    await expect(handled.promise).resolves.toBe(4);
+    await expect(handled.promise).resolves.toEqual({ value: 4, index: 0 });
   });
 
   it("handles Promises that reject (later)", async () => {
     const handled = new HandledRejectionPromise(
       new Promise((_resolve, reject) =>
         setTimeout(() => reject(new Error("ok")), 1)
-      )
+      ),
+      0
     );
 
     await expect(handled.promise).rejects.toThrowErrorMatchingInlineSnapshot(
